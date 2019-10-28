c5ad44c74240498d2c88912d7327e1d3b8dddd03
song6576 <46815290+song6576@users.noreply.github.com>
2019-10-28 22:28:17 +0800

    景院裹裹布局
    微信小程序景院裹裹

--- mini_guoguo/pages/index/index.js
+++ mini_guoguo/pages/index/index.js
@@ -2,53 +2,42 @@
 //获取应用实例
 const app = getApp()
 
-Page({
+// Page({
+//   // 页面的初始数据
+//   data: {
+//     amount: "911",
+//     receive: "1",
+//     account: "18"
+//   },
+//   // 搜索页面跳转
+//   sousuoClick:function(){
+//     wx.navigateTo({
+//       url: '../input-sousuo/input-sousuo'
+//     })
+//   }
+// })
+
+Component({
   data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+    amount: "911",
+    receive: "1",
+    account: "18"
   },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
-  },
-  onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
+  methods: {
+    handleClick: function () {
+      wx.navigateTo({
+        url: '../input-sousuo/input-sousuo'
       })
     }
   },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
-    })
+  //页面将tabBar中的selected记录值更改，达到tabBar页面跳转图标更换
+  pageLifetimes: {
+    show() {
+      this.getTabBar().setData({
+        // 跳转首页
+        selected: 0
+      });
+    }
   }
 })
+
